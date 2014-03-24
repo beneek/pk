64ebef89b0389ce67ccfc240e8c74cbb51ee6995
benek <bartlomiej.pokrzywinski@gmail.com>
2014-03-24 18:46:09 +0100

    poprawiony projekt 4

--- MO/Projekt4/Projekt4.c
+++ MO/Projekt4/Projekt4.c
@@ -25,7 +25,7 @@ double funkcja2(double x, double y){
 //////////////////////////////////////////////////////////////////////
 void MetNew(double (*f1)(double,double), double (*f2)(double,double)){
 	int i = 0;
-	double Jakobi[2][2];
+	double Jakobi[2][2],JakobiT[2][2];
 	double Przyb[2] = { 0 , sqrt(8)};
 	double Wynik[2];
 	double WyzJ,WyzJx,WyzJy,fx,fy;
@@ -40,24 +40,26 @@ void MetNew(double (*f1)(double,double), double (*f2)(double,double)){
 
 		//Liczenie wyznacznika macierzy Jacobiego
 		WyzJ = Jakobi[0][0] * Jakobi[1][1] - Jakobi[1][0]*Jakobi[0][1];
-		WyzJx = fx * Jakobi[1][1] - fy *Jakobi[0][1];
-		WyzJy = Jakobi[0][0] * fy - Jakobi[1][0] * fx;
+		JakobiT[0][0] = Jakobi[1][1]/WyzJ;
+		JakobiT[0][1] = -Jakobi[0][1]/WyzJ;
+		JakobiT[1][0] = -Jakobi[1][0]/WyzJ;
+		JakobiT[1][1] = Jakobi[0][0]/WyzJ;
 
-		Wynik[0] = WyzJx / WyzJ;
-		Wynik[1] = WyzJy / WyzJ;
+		Wynik[0] = JakobiT[0][0] * fx + JakobiT[0][1] * fy;
+		Wynik[1] = JakobiT[1][0] * fx + JakobiT[1][1] * fy;
 
 		//Nastepne przyblizenie
 		Przyb[0]-=Wynik[0];
 		Przyb[1]-=Wynik[1];
 		
-		printf("%d-> x:%lf\ty:%lf\tf1(x,y):%lf\tf2(x,y):%lf\n",i,Przyb[0],Przyb[1],fx,fy);
+		printf("%d-> x:%lf\ty:%lf\tf1(x,y):%.25lf\tf2(x,y):%.25lf\n",i,Przyb[0],Przyb[1],fx,fy);
 		i++;
 		if(fabs(fx) < EPS && fabs(fy) < EPS){
-			printf("Osiagnieto odpowiednia dokladnosc dla wartosci funkcji, koncze zadanie\n");
+			printf("Osiagnieto zadana dokladnosc dla wartosci funkcji, koncze zadanie\n");
 			return;
 		}
 		if(fabs(Wynik[0]) < EPS && fabs(Wynik[1]) < EPS){
-			printf("Osiagnieto odpowiednia dokladnosc dla wektora, koncze zadanie\n");
+			printf("Osiagnieto zadana dokladnosc dla wektora, koncze zadanie\n");
 			return;
 		}
 	}while(i <= MAX_ITER);
