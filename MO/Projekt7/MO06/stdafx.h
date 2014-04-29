6d32988b4fb015d0fe078b18f4d42272e01b23c7
benek <bartlomiej.pokrzywinski@gmail.com>
2014-04-29 10:32:37 +0200

    projekt 7 MO

---
+++ MO/Projekt7/MO06/stdafx.h
@@ -0,0 +1,26 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <string>
+#define DIM 2
+//Tolerancja dla estymatora
+#define EPS 1e-7
+
+//Tolerancja dla wektora residualnego
+#define VECTEPS 1e-10
+
+#define MAX_ITER 100
+using namespace std;
+
+
+
+// TODO: reference additional headers your program requires here
